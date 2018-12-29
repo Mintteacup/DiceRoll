7433d282081276c8f0bf7ab24f8e7c680b509bd5
Mint <35158823+Mintteacup@users.noreply.github.com>
2018-12-29 14:07:55 +0000

    Add files via upload

---
+++ Dice Roll.py
@@ -0,0 +1,14 @@
+import random
+sides = 0
+rolls = 0
+roll_count = 0
+
+print("This program is a simple dice roll program.")
+input("press enter to continue...")
+
+sides = int(input("How many sides do you want the dice to be? "))
+rolls = int(input("How many dice do you want to roll? "))
+
+while roll_count < rolls:
+    print(random.randint(1, sides))
+    roll_count += 1
