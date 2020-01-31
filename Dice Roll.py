3ea799d81ebdd66b350296e60cc4e1c6b17545a2
Mint <35158823+Mintteacup@users.noreply.github.com>
2020-01-31 13:25:21 +0000

    Update Dice Roll.py
    changed a word

--- Dice Roll.py
+++ Dice Roll.py
@@ -6,7 +6,7 @@ roll_count = 0
 print("This program is a simple dice roll program.")
 input("press enter to continue...")
 
-sides = int(input("How many sides do you want the dice to be? "))
+sides = int(input("How many sides do you want the dice to have? "))
 rolls = int(input("How many dice do you want to roll? "))
 
 while roll_count < rolls:
